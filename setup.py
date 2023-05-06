634325113f66d95412f8d5107505853bf0e28787
jnaederSAE <j.naeder@sae.edu>
2023-05-06 00:09:49 -0400

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sae_toolkit',
+    version='0.1.0',
+    description='Reusable Tools for SAE Development',
+    author='John Naeder',
+    author_email='j.naeder@sae.edu',
+    url='https://github.com/yourusername/your_package_name',
+    packages=find_packages(),
+    install_requires=[]
+)
