4b424fd083975d84d05d8032a5b57da8ed461cff
jnaederSAE <j.naeder@sae.edu>
2023-05-07 15:22:54 -0400

    Changes to google sheets

--- sae_toolkit/google_sheets.py
+++ sae_toolkit/google_sheets.py
@@ -11,26 +11,14 @@ def parse_dataframe(dataframe: pd.DataFrame) -> list:
     return output
 
 
-class GoogleSheets:
-    """Class for importing and exporting data to Google Sheets."""
-
-    def __init__(self, sheet_id: str) -> None:
+class SpreadSheet:
+    def __init__(self, sheet_id, service):
         self._sheet_id = sheet_id
-        self._scopes = ['https://www.googleapis.com/auth/spreadsheets']
-        self.current_file = os.path.abspath(__file__)
-        self.current_dir = os.path.dirname(self.current_file)
-        self.cred_file = 'google_credentials.json'
-        self._account_file = os.path.join(self.current_dir, self.cred_file)
-        self._creds = service_account.Credentials.from_service_account_file(self._account_file,
-                                                                            scopes=self._scopes)
-        self._service = build('sheets', 'v4', credentials=self._creds)
         self._value_input_option = "RAW"
         self._insert_data_option = "OVERWRITE"
+        self._service = service
 
-    def set_google_credentials(self, path_to_file: str) -> None:
-        self._account_file = path_to_file
-
-    def get_data_by_worksheet_name(self, worksheet_name: str, sheet_range: str = None) -> list:
+    def get_worksheet_data(self, worksheet_name: str, sheet_range: str = None) -> list:
         worksheet_range = f"!{sheet_range}" if sheet_range else ""
         request = self._service.spreadsheets().values().get(spreadsheetId=self._sheet_id,
                                                             range=f"{worksheet_name}{worksheet_range}")
@@ -53,3 +41,17 @@ class GoogleSheets:
                                                                valueInputOption=self._value_input_option,
                                                                insertDataOption=self._insert_data_option, body=body)
         request.execute()
+
+
+class GoogleSheets:
+
+    def __init__(self, credential_file) -> None:
+        self._scopes = ['https://www.googleapis.com/auth/spreadsheets']
+        self.current_file = os.path.abspath(__file__)
+        self.current_dir = os.path.dirname(self.current_file)
+        self._account_file = os.path.join(self.current_dir, credential_file)
+        self._creds = service_account.Credentials.from_service_account_file(self._account_file, scopes=self._scopes)
+        self._service = build('sheets', 'v4', credentials=self._creds)
+
+    def spreadsheet(self, sheet_id):
+        return SpreadSheet(sheet_id, self._service)
